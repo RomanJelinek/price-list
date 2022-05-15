0ef5b8e43a4d5a72449682f84f839a557b476d68
RomanJelinek <roman.jelinek91@gmail.com>
2022-05-15 15:38:55 +0200

    disabled mobile zoom

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap" rel="stylesheet"/>
+        <meta name="viewport" 
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
           </Head>
       <body>
         <Main />

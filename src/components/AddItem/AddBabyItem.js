eeb9ed187fe6868b341576b546a1bf241e1e7d7f
RomanJelinek <roman.jelinek91@gmail.com>
2022-05-15 15:29:18 +0200

    added loading info

--- src/components/AddItem/AddBabyItem.js
+++ src/components/AddItem/AddBabyItem.js
@@ -13,6 +13,7 @@ const AddBabyItem = ({ updateItems, text, selectedItem }) => {
     value: 0,
     color: 0,
   });
+  const [sending, setSending] = useState(false);
 
   useEffect(() => {
     if (text.type === 'update') {
@@ -33,6 +34,7 @@ const AddBabyItem = ({ updateItems, text, selectedItem }) => {
     const type = isDelete ? isDelete : text.type;
     const valuesToSend = { type: type, itemToUpdate, values };
     if (values.title !== '' && values.value > 0) {
+      setSending(true);
       showError && setShowError(false);
       try {
         const resp = await axios.post('/api/insert-values', valuesToSend);
@@ -41,6 +43,7 @@ const AddBabyItem = ({ updateItems, text, selectedItem }) => {
         // Handle Error Here
         console.error(err);
       } finally {
+        setSending(false);
         updateItems(type, values, itemToUpdate);
       }
     } else {
@@ -51,35 +54,41 @@ const AddBabyItem = ({ updateItems, text, selectedItem }) => {
   return (
     <>
       <h1>{text.headline}</h1>
-        <li>
-          {text.item}{' '}
-          <input name="title" value={values.title} onChange={getValues}></input>
-        </li>
-        <li>
-          {text.addAmount}
-          <input
-            type="number"
-            name="value"
-            value={values.value}
-            onChange={getValues}
-          ></input>
-        </li>
-        <button
-          onClick={() => sendResults('')}
-          style={{ background: '#8cff9e', cursor: 'pointer' }}
-        >
-          {text.sendButton}
-        </button>
-       {text.type === "update" && <button
-          onClick={() => sendResults('delete')}
-          style={{
-            background: '#CD5C5C',
-            cursor: 'pointer',
-            marginTop: '20px',
-          }}
-        >
-          Smazat
-        </button>}
+      <li>
+        {text.item}{' '}
+        <input name="title" value={values.title} onChange={getValues}></input>
+      </li>
+      <li>
+        {text.addAmount}
+        <input
+          type="number"
+          name="value"
+          value={values.value}
+          onChange={getValues}
+        ></input>
+      </li>
+      {!sending ?
+        <>
+          <button
+            onClick={() => sendResults('')}
+            style={{ background: '#8cff9e', cursor: 'pointer' }}
+          >
+            {text.sendButton}
+          </button>
+          {text.type === 'update' && (
+            <button
+              onClick={() => sendResults('delete')}
+              style={{
+                background: '#CD5C5C',
+                cursor: 'pointer',
+                marginTop: '20px',
+              }}
+            >
+              Smazat
+            </button>
+          )}
+        </> : <h3>Odesílám ...</h3>
+      }
       {showError && <span style={{ color: 'red' }}>{text.error}</span>}
     </>
   );
